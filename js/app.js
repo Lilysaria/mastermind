ad634a4c0448e416796579f3fb041f2d64d53ec9
saria <bloomyday56@gmail.com>
2024-02-06 12:45:45 -0800

    trying to figure out the function and math.random set up that will generate the random secret code when the page loads

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
+document.addEventListener('DOMContentLoaded', function() {
+let secretCode = generateSecretCode();
 let currentHoleIndex = 0;
 /*----- cached element references -----*/
 let holes = document.querySelectorAll('.hole');
@@ -13,17 +15,28 @@ let pegs = document.querySelectorAll('.peg');
                 coloredPeg.className = 'colored-peg';
                 //coloredPeg represents the peg going in the hole
                 coloredPeg.style.backgroundColor = getComputedStyle(peg).backgroundColor;
+                //getComputedStyle retrieves the color of the clicked peg
                 holes[currentHoleIndex].innerHTML = '';
                 holes[currentHoleIndex].appendChild(coloredPeg);
+                //after handling the click, below code tell it to move to the next hole
                 currentHoleIndex++;
                 if (currentHoleIndex === holes.length) {
                     currentHoleIndex = 0;
                 }
 });
 });
-
+});
 
 /*----- functions -----*/
+function generateSecretCode() {
+    const colors = ['green', 'yellow','purple','teal','red', 'blue'];
+    let secretCode = [];
 
+    for (let i = 0; i < 4; i++) {
+        let randomColor = colors[Math.floor(Math.random() * colors.length)]
+        secretCode.push(randomColor);
+    }
+    return secretCode;
+}
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
